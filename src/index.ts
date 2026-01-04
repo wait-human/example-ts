daf3828d3582e5da9f48bab843517e4d239dfd8d
Giovanni <gfeltrin25@gmail.com>
2026-01-04 18:33:53 +0100

    Update example to latest version

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import { WaitHuman } from "@wait-human/client";
 
 const waitHuman = new WaitHuman({
-    apiKey: "sk_4101d7ff12d8328c9322526a125115ac7ce7d379b3f089d3",
-    endpoint: "http://localhost:8080",
+    apiKey: "your_key",
 });
 
 const answer = await waitHuman.askMultipleChoice({
